56cbe1dd158e0ebb5fbc2d3eb4176d041c3f552d
farahmed@gmail.com <farahmed@gmail.com>
2018-03-31 11:52:45 -0500

    cleaning tree

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {RoomList} from './components/RoomList';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDa1WlNms3Esx6pVWf6KMgxVrb6wOJs9wA",
+  authDomain: "bloc-chat-23568.firebaseapp.com",
+  databaseURL: "https://bloc-chat-23568.firebaseio.com",
+  projectId: "bloc-chat-23568",
+  storageBucket: "bloc-chat-23568.appspot.com",
+  messagingSenderId: "323263946081"
+};
+firebase.initializeApp(config);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <RoomList/>
+      </div>
+    );
+  }
+}
+
+export default App;
